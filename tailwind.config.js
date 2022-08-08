7f4855e25973ca02cdff991a419e65ee6c7f0fc9
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-08 16:27:52 +0500

    Colors and Fonts Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Vivid Red-Tangelo": "#D85C27",
+        "Medium-Electric-Blue": "#085394",
+        Black: "#000000",
+        White: "#FFFFFF",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
