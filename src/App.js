f4d9fd15ed15fec38edb460193dfeeafc4f52f6b
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-11 20:50:46 +0500

    New route for deskop configures

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import BookTable from "./pages/BookTable/index.js";
+import Dashboard from "./pages/DashBoard";
+
 import "./App.css";
 
 // React Slick CSS
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import BookTable from "./pages/BookTable.js";
 
 function App() {
   return (
-    <div className="font-Montserrat scroll-smooth	 ">
+    <div className="font-Montserrat">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* Route for Desktop */}
+          <Route path="/dashboard" element={<Dashboard />} />
 
           {/* Route for mobile phone */}
           <Route path="/booktable" element={<BookTable />} />
