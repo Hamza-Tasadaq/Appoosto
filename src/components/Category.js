ced4f8943aaa34ea2ceb0f532573ad0e40751860
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-14 02:59:54 +0500

    Dashboard page completed

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,33 @@
+import MenuData from "../data/Category.json";
+import CategoryCard from "./CategoryCard";
+
 const Category = () => {
+  const half = Math.ceil(MenuData.length / 2);
+  const firstHalf = MenuData.slice(0, half);
+  const secondHalf = MenuData.slice(half);
   return (
     <div className="relative">
-      <div>
-        Category
+      <div className="flex flex-col overflow-x-auto noScrollBar">
+        <div className="flex items-center my-1 space-x-3">
+          {firstHalf.map(({ name, iconSrc, background }, index) => (
+            <CategoryCard
+              key={index}
+              background={background}
+              name={name}
+              iconSrc={iconSrc}
+            />
+          ))}
+        </div>
+        <div className="flex items-center my-2 space-x-3">
+          {secondHalf.map(({ name, iconSrc, background }, index) => (
+            <CategoryCard
+              key={index}
+              background={background}
+              name={name}
+              iconSrc={iconSrc}
+            />
+          ))}
+        </div>
       </div>
       <div className="bg-Spanish-Gray  h-px max-w-[60%] mx-auto opacity-25" />
     </div>
---
+++ src/components/CategoryCard.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const hex2rgba = (hex, alpha = 1) => {
+  const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
+  return `rgba(${r},${g},${b},${alpha})`;
+};
+
+const CategoryCard = ({ name, iconSrc, background }) => {
+  const [isSelected, setIsSelected] = useState(false);
+  return (
+    <div
+      onClick={() => {
+        setIsSelected(!isSelected);
+      }}
+      style={
+        isSelected
+          ? { backgroundColor: `${hex2rgba(background, 0.6)}` }
+          : { backgroundColor: `${hex2rgba(background, 0.2)}` }
+      }
+      className={`rounded-lg flex flex-col items-center justify-center space-y-3 cursor-pointer min-w-[166px] min-h-[115px] py-3 px-5 duration-300 hover:bg-opacity-60`}
+    >
+      <img src={`./assets/${iconSrc}.svg`} alt={iconSrc} />
+      <h1 className="font-bold text-sm">{name}</h1>
+    </div>
+  );
+};
+
+export default CategoryCard;
