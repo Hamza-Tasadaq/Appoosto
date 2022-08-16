b089b3299c76ef624dd1690f8ba9ffe060cfaac1
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-16 21:05:29 +0500

    Flag Icon Added

--- src/pages/BookTable/index.js
+++ src/pages/BookTable/index.js
@@ -4,9 +4,12 @@ import { BookTableForm } from "../../components";
 const BookTable = () => {
   return (
     <div>
-      <Link to={"/"} className="rounded-b-lg shadow-lg p-4 flex items-center sticky top-0 left-0 right-0 z-50 bg-White">
-        <img src="./assets/headers-logo-gray.png" alt="headers-logo-gray" />
-      </Link>
+      <div className="rounded-b-lg shadow-lg p-4 flex items-center sticky top-0 left-0 right-0 z-50 bg-White justify-between">
+        <Link to={"/"}>
+          <img src="./assets/headers-logo-gray.png" alt="headers-logo-gray" />
+        </Link>
+        <img src="./assets/flag.png" alt="flag" />
+      </div>
       <BookTableForm />
     </div>
   );
