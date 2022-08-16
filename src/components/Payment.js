b1d3aaf0976e59dbecd1a31a2c314d65ce7f30de
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-17 03:02:47 +0500

    Payment Method Delete

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -75,33 +75,34 @@ const Payment = () => {
         </div>
 
         <div className="md:hidden bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
+        {!selectedMethod.cash && (
+          <div className="space-y-2 md:my-4">
+            <input
+              className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White text-xs md:bg-White"
+              placeholder="Name on card"
+            />
 
-        <div className="space-y-2 md:my-4">
-          <input
-            className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White text-xs md:bg-White"
-            placeholder="Name on card"
-          />
-
-          <div className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White md:bg-White flex flex-row md:flex-col lg:flex-row items-center">
-            <img src="./assets/credit-card.svg" alt="credit-card" />
-            <div className="flex flex-row md:flex-col lg:flex-row items-center text-xs justify-between flex-1">
-              <input
-                className="bg-transparent outline-none w-20 md:w-28 ml-2 md:my-1 lg:my-0 md:ml-0 lg:ml-2"
-                placeholder="Card number"
-              />
-              <div>
+            <div className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White md:bg-White flex flex-row md:flex-col lg:flex-row items-center">
+              <img src="./assets/credit-card.svg" alt="credit-card" />
+              <div className="flex flex-row md:flex-col lg:flex-row items-center text-xs justify-between flex-1">
                 <input
-                  className="bg-transparent outline-none max-w-[70px] mr-2 md:my-1 lg:my-0  border-r md:border-none lg:border-r border-Spanish-Gray"
-                  placeholder="MM/AA"
+                  className="bg-transparent outline-none w-20 md:w-28 ml-2 md:my-1 lg:my-0 md:ml-0 lg:ml-2"
+                  placeholder="Card number"
                 />
-                <input
-                  className="bg-transparent outline-none max-w-[40px] ml-2 md:my-1 md:ml-0 lg:ml-0 lg:my-0"
-                  placeholder="CVC"
-                />
+                <div>
+                  <input
+                    className="bg-transparent outline-none max-w-[70px] mr-2 md:my-1 lg:my-0  border-r md:border-none lg:border-r border-Spanish-Gray"
+                    placeholder="MM/AA"
+                  />
+                  <input
+                    className="bg-transparent outline-none max-w-[40px] ml-2 md:my-1 md:ml-0 lg:ml-0 lg:my-0"
+                    placeholder="CVC"
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
         <div className="hidden md:block bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
       </div>
 
