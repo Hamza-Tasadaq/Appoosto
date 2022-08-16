85b9ab86cb384ffd58dd2487acd03e742a09e552
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-16 20:54:33 +0500

    Logo Change

--- src/pages/BookTable/index.js
+++ src/pages/BookTable/index.js
@@ -5,8 +5,7 @@ const BookTable = () => {
   return (
     <div>
       <Link to={"/"} className="rounded-b-lg shadow-lg p-4 flex items-center sticky top-0 left-0 right-0 z-50 bg-White">
-        <img src="./assets/booktable-logo.png" alt="booktable-logo" />
-        <h3 className="font-semibold text-sm ml-3">Pan Asian Restaurant</h3>
+        <img src="./assets/headers-logo-gray.png" alt="headers-logo-gray" />
       </Link>
       <BookTableForm />
     </div>
