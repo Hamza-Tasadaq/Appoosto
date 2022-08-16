53a55d6449e79600282e72efdc4f11e58f4c6f27
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-16 21:31:03 +0500

    Keyboard Button Fixed

--- src/pages/PinScreen/index.js
+++ src/pages/PinScreen/index.js
@@ -1,6 +1,29 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+const KeyboardButton = ({ clickHandle = () => {}, number }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handlerClick = () => {
+    setIsClicked(true);
+    clickHandle(number);
+
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 500);
+  };
+  return (
+    <h1
+      onClick={handlerClick}
+      className={`font-semibold text-3xl py-2 w-[100px] my-3 px-10 rounded-lg ${
+        isClicked && "bg-White text-Vivid-Red-Tangelo"
+      } `}
+    >
+      {number}
+    </h1>
+  );
+};
+
 function PinScreen() {
   const navigate = useNavigate();
   // Otp input text
@@ -47,8 +70,7 @@ function PinScreen() {
     <>
       <div className="sticky top-0 left-0 right-0 z-50 bg-White rounded-b-lg shadow-lg p-4 flex items-center justify-between">
         <Link to={"/"} className=" flex items-center ">
-          <img src="./assets/booktable-logo.png" alt="booktable-logo" />
-          <h3 className="font-semibold text-sm ml-3">Pan Asian Restaurant</h3>
+          <img src="./assets/headers-logo-gray.png" alt="headers-logo-gray" />
         </Link>
         <img src="./assets/flag.png" alt="flag" />
       </div>
@@ -92,13 +114,7 @@ function PinScreen() {
       <div className="w-full rounded-t-lg z-50 fixed bottom-0 left-0  bg-Vivid-Red-Tangelo py-3 flex items-center mt-5 justify-center">
         <div className="flex justify-center items-center flex-wrap text-White max-w-sm	">
           {Array.from({ length: 9 }, (_, i) => (
-            <h1
-              key={i}
-              onClick={() => handleClick(i + 1)}
-              className="font-semibold text-3xl py-2 w-[100px] my-3 px-10 rounded-lg hover:bg-White hover:text-Vivid-Red-Tangelo"
-            >
-              {i + 1}
-            </h1>
+            <KeyboardButton key={i} number={i + 1} clickHandle={handleClick} />
           ))}
           <div
             onClick={handleClickBack}
@@ -108,12 +124,8 @@ function PinScreen() {
               <img src="./assets/arrow-backward.svg" alt="arrow-backward" />
             </div>
           </div>
-          <h1
-            onClick={() => handleClick(0)}
-            className="font-semibold text-3xl py-2 w-[100px] my-3 px-10 rounded-lg hover:bg-White hover:text-Vivid-Red-Tangelo"
-          >
-            0
-          </h1>
+
+          <KeyboardButton number={0} clickHandle={handleClick} />
 
           <div
             onClick={handleValidation}
