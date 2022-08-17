2d8c294b6367bceea9b434cd62734d6adfe7df6b
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-17 16:00:09 +0500

    Refactor

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { useDispatch } from "react-redux";
