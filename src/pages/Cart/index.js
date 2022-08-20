74514fa3d2daa64642d3906a4151c178e33fa1c6
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-20 23:54:10 +0500

    Cart Empty Screen Done

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { Cart as ProductCart, Payment } from "../../components";
 
 const Cart = () => {
@@ -6,8 +7,30 @@ const Cart = () => {
 
   return (
     <>
-      <ProductCart />
-      {cart.length > 0 && <Payment />}
+      {cart.length > 0 ? (
+        <>
+          <ProductCart />
+          <Payment />
+        </>
+      ) : (
+        <div className="w-screen h-screen bg-black p-10 flex flex-col justify-between items-center">
+          <div className="text-White text-center space-y-4">
+            <img src="./assets/empty.png" alt="empty" />
+            <h1 className="font-bold text-base">Your cart is empty!</h1>
+
+            <p className="max-w-[255px] font-medium">
+              Visit order page to create your order
+            </p>
+          </div>
+
+          <Link
+            className="text-White bg-Vivid-Red-Tangelo rounded-xl py-4 px-10"
+            to="/menu"
+          >
+            visit order page
+          </Link>
+        </div>
+      )}
     </>
   );
 };
