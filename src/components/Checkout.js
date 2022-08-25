00f5dc7e7a09bdc248a7d42f125660b216be120c
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-26 01:07:52 +0500

    CheckOut Updated

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,8 +6,11 @@ const Checkout = () => {
   const [showDropDown, setShowDropDown] = useState(false);
   const { selectedOption } = useSelector((state) => state.grid);
 
-  const dropDownClickHandle = () => {
+  const [selectedTime, setSelectedTime] = useState(undefined);
+
+  const dropDownClickHandle = (time = "") => {
     setShowDropDown(!showDropDown);
+    setSelectedTime(time);
   };
   return (
     <div className="bg-White min-h-full rounded-lg p-2">
@@ -19,10 +22,45 @@ const Checkout = () => {
             <h3 className="font-semibold text-xs">Name</h3>
             <input className="bg-Light-Silver bg-opacity-50 my-1 rounded-lg w-full p-2 outline-none" />
           </div>
-          <div className="my-2">
-            <h3 className="font-semibold text-xs">Delivery address</h3>
-            <input className="bg-Light-Silver bg-opacity-50 my-1 rounded-lg w-full p-2 outline-none" />
-          </div>
+          {selectedOption === "Delivery" && (
+            <div className="my-2">
+              <h3 className="font-semibold text-xs">Delivery address</h3>
+              <input className="bg-Light-Silver bg-opacity-50 my-1 rounded-lg w-full p-2 outline-none" />
+            </div>
+          )}
+
+          {selectedOption === "Take Away" && (
+            <div className="my-2">
+              <h3 className="font-semibold text-xs">Pickup time</h3>
+              <div>
+                <div
+                  onClick={() => dropDownClickHandle()}
+                  className="bg-Light-Silver bg-opacity-50 px-5 mt-3 mb-1 py-4 rounded-lg flex items-center justify-between cursor-pointer"
+                >
+                  <h3 className="font-semibold text-xs">{selectedTime}</h3>
+                  <img src="./assets/arrow-down.svg" alt="arrow-down" />
+                </div>
+                <div
+                  className={`px-5 py-4 shadow-lg	 bg-Light-Silver bg-opacity-50 space-y-2 transition-all	 duration-500 ${
+                    showDropDown ? " block	" : " hidden"
+                  } `}
+                >
+                  <h3
+                    onClick={() => dropDownClickHandle("11:00 - 12:30")}
+                    className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+                  >
+                    11:00 - 12:30
+                  </h3>
+                  <h3
+                    onClick={() => dropDownClickHandle("12:0 - 14:00")}
+                    className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+                  >
+                    12:0 - 14:00
+                  </h3>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       ) : (
         <div className="hidden md:block bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
@@ -34,31 +72,33 @@ const Checkout = () => {
 
 export default Checkout;
 
-// <h2 className="opacity-50 font-semibold text-xs">Pickup time</h2>
-// <div>
-//   <div
-//     onClick={dropDownClickHandle}
-//     className="bg-Light-Silver bg-opacity-50 px-5 mt-3 mb-1 py-4 rounded-lg flex items-center justify-between cursor-pointer"
-//   >
-//     <h3 className="font-semibold text-xs">Time</h3>
-//     <img src="./assets/arrow-down.svg" alt="arrow-down" />
-//   </div>
-//   <div
-//     className={`px-5 py-4 shadow-lg	 bg-Light-Silver bg-opacity-50 space-y-2 transition-all	 duration-500 ${
-//       showDropDown ? " block	" : " hidden"
-//     } `}
-//   >
-//     <h3
-//       onClick={dropDownClickHandle}
-//       className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
-//     >
-//       11:00 - 12:30
-//     </h3>
-//     <h3
-//       onClick={dropDownClickHandle}
-//       className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
-//     >
-//       12:0 - 14:00
-//     </h3>
-//   </div>
-// </div>
+{
+  /* <h2 className="opacity-50 font-semibold text-xs">Pickup time</h2>
+<div>
+  <div
+    onClick={dropDownClickHandle}
+    className="bg-Light-Silver bg-opacity-50 px-5 mt-3 mb-1 py-4 rounded-lg flex items-center justify-between cursor-pointer"
+  >
+    <h3 className="font-semibold text-xs">Time</h3>
+    <img src="./assets/arrow-down.svg" alt="arrow-down" />
+  </div>
+  <div
+    className={`px-5 py-4 shadow-lg	 bg-Light-Silver bg-opacity-50 space-y-2 transition-all	 duration-500 ${
+      showDropDown ? " block	" : " hidden"
+    } `}
+  >
+    <h3
+      onClick={dropDownClickHandle}
+      className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+    >
+      11:00 - 12:30
+    </h3>
+    <h3
+      onClick={dropDownClickHandle}
+      className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+    >
+      12:0 - 14:00
+    </h3>
+  </div>
+</div> */
+}
