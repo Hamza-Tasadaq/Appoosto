426d13aa4d8f6ac7e0d647b30beb1b96523f6462
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2022-08-26 00:58:08 +0500

    Checkout implemented

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,51 +1,64 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import Payment from "./Payment";
 
 const Checkout = () => {
   const [showDropDown, setShowDropDown] = useState(false);
+  const { selectedOption } = useSelector((state) => state.grid);
 
   const dropDownClickHandle = () => {
     setShowDropDown(!showDropDown);
   };
   return (
-    <div>
+    <div className="bg-White min-h-full rounded-lg p-2">
       <h1 className="text-center font-bold text-lg">Checkout</h1>
-      <div className="bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-2" />
 
-      <h2 className="opacity-50 font-semibold text-xs">Pickup time</h2>
-      <div>
-        <div
-          onClick={dropDownClickHandle}
-          className="bg-White px-5 mt-3 mb-1 py-4 rounded-lg flex items-center justify-between cursor-pointer"
-        >
-          <h3 className="font-semibold text-xs">Time</h3>
-          <img src="./assets/arrow-down.svg" alt="arrow-down" />
+      {selectedOption === "Delivery" || selectedOption === "Take Away" ? (
+        <div className="my-4">
+          <div className="my-2">
+            <h3 className="font-semibold text-xs">Name</h3>
+            <input className="bg-Light-Silver bg-opacity-50 my-1 rounded-lg w-full p-2 outline-none" />
+          </div>
+          <div className="my-2">
+            <h3 className="font-semibold text-xs">Delivery address</h3>
+            <input className="bg-Light-Silver bg-opacity-50 my-1 rounded-lg w-full p-2 outline-none" />
+          </div>
         </div>
-        <div
-          className={`px-5 py-4 shadow-lg	 bg-white space-y-2 transition-all	 duration-500 ${
-            showDropDown ? " block	" : " hidden"
-          } `}
-        >
-          <h3
-            onClick={dropDownClickHandle}
-            className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
-          >
-            11:00 - 12:30
-          </h3>
-          <h3
-            onClick={dropDownClickHandle}
-            className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
-          >
-            12:0 - 14:00
-          </h3>
-        </div>
-      </div>
-
-      <div className="bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
-
-      <Payment />
+      ) : (
+        <div className="hidden md:block bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
+      )}
+      {selectedOption && <Payment />}
     </div>
   );
 };
 
 export default Checkout;
+
+// <h2 className="opacity-50 font-semibold text-xs">Pickup time</h2>
+// <div>
+//   <div
+//     onClick={dropDownClickHandle}
+//     className="bg-Light-Silver bg-opacity-50 px-5 mt-3 mb-1 py-4 rounded-lg flex items-center justify-between cursor-pointer"
+//   >
+//     <h3 className="font-semibold text-xs">Time</h3>
+//     <img src="./assets/arrow-down.svg" alt="arrow-down" />
+//   </div>
+//   <div
+//     className={`px-5 py-4 shadow-lg	 bg-Light-Silver bg-opacity-50 space-y-2 transition-all	 duration-500 ${
+//       showDropDown ? " block	" : " hidden"
+//     } `}
+//   >
+//     <h3
+//       onClick={dropDownClickHandle}
+//       className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+//     >
+//       11:00 - 12:30
+//     </h3>
+//     <h3
+//       onClick={dropDownClickHandle}
+//       className="p-2 cursor-pointer opacity-50 rounded-lg hover:bg-Chinese-Silver"
+//     >
+//       12:0 - 14:00
+//     </h3>
+//   </div>
+// </div>
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -8,6 +8,7 @@ const Payment = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { cart } = useSelector((state) => state.cart);
+  const { selectedOption } = useSelector((state) => state.grid);
   const [selectedMethod, setSelectedMethod] = useState({
     cash: true,
     card: false,
@@ -28,102 +29,104 @@ const Payment = () => {
       cart.map((cartItem) => {
         return { ...cartItem, status: "Prepration", time: "2:30" };
       }),
-    ]
+    ];
 
     dispatch(addOrder(...OrdersStatus));
   };
   return (
     <div>
-      <div className="mx-4  p-4 md:p-0 md-p-0 md:mx-0 mb-24 md:mb-4 md:my-4 bg-White md:bg-transparent rounded-lg shadow-lg md:shadow-none">
+      <div className="mx-4  p-4 md:p-0 md-p-0 md:mx-0 mb-16 md:my-3 bg-White md:bg-transparent rounded-lg shadow-lg md:shadow-none">
         <div className="md:hidden">
           <h2 className="opacity-30 py-2 border-b border-Spanish-Gray">
             Payment
           </h2>
         </div>
-
-        <div className="pt-4 md:py-0 space-y-1 text-xs md:text-base ">
-          <div
-            onClick={() => {
-              handlePaymentSelect("cash");
-            }}
-            className="flex items-center cursor-pointer"
-          >
-            <div
-              className={`${
-                selectedMethod.cash
-                  ? "  border-Medium-Electric-Blue md:border-Vivid-Red-Tangelo "
-                  : " border-Spanish-Gray"
-              } rounded-full border-2 w-5 h-5 flex items-center justify-center`}
-            >
+        {selectedOption === "Delivery" || selectedOption === "Take Away" ? (
+          <div>
+            <div className="pt-4 md:py-0 space-y-1 text-xs md:text-base ">
               <div
-                className={`${
-                  selectedMethod.cash
-                    ? " w-3 h-3 bg-Medium-Electric-Blue md:bg-Vivid-Red-Tangelo "
-                    : " bg-transparent"
-                } rounded-full`}
-              />
-            </div>
-            <h3 className="font-semibold ml-3">Cash/Card to terminal</h3>
-          </div>
-          <div
-            onClick={() => {
-              handlePaymentSelect("card");
-            }}
-            className="flex items-center cursor-pointer"
-          >
-            <div
-              className={`${
-                selectedMethod.card
-                  ? "  border-Medium-Electric-Blue md:border-Vivid-Red-Tangelo "
-                  : " border-Spanish-Gray"
-              } rounded-full border-2 w-5 h-5 flex items-center justify-center`}
-            >
-              <div
-                className={`${
-                  selectedMethod.card
-                    ? " w-3 h-3 bg-Medium-Electric-Blue md:bg-Vivid-Red-Tangelo "
-                    : " bg-transparent"
-                } rounded-full`}
-              />
-            </div>
-            <h3 className="font-semibold ml-3">Pay now with card</h3>
-          </div>
-        </div>
-
-        <div className="md:hidden bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
-        {!selectedMethod.cash && (
-          <div className="space-y-2 md:my-4">
-            <input
-              className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White text-xs md:bg-White"
-              placeholder="Name on card"
-            />
-
-            <div className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White md:bg-White flex flex-row md:flex-col lg:flex-row items-center">
-              <img src="./assets/credit-card.svg" alt="credit-card" />
-              <div className="flex flex-row md:flex-col lg:flex-row items-center text-xs justify-between flex-1">
-                <input
-                  className="bg-transparent outline-none w-20 md:w-28 ml-2 md:my-1 lg:my-0 md:ml-0 lg:ml-2"
-                  placeholder="Card number"
-                />
-                <div>
-                  <input
-                    className="bg-transparent outline-none max-w-[70px] mr-2 md:my-1 lg:my-0  border-r md:border-none lg:border-r border-Spanish-Gray"
-                    placeholder="MM/AA"
-                  />
-                  <input
-                    className="bg-transparent outline-none max-w-[40px] ml-2 md:my-1 md:ml-0 lg:ml-0 lg:my-0"
-                    placeholder="CVC"
+                onClick={() => {
+                  handlePaymentSelect("cash");
+                }}
+                className="flex items-center cursor-pointer"
+              >
+                <div
+                  className={`${
+                    selectedMethod.cash
+                      ? "  border-Medium-Electric-Blue md:border-Vivid-Red-Tangelo "
+                      : " border-Spanish-Gray"
+                  } rounded-full border-2 w-5 h-5 flex items-center justify-center`}
+                >
+                  <div
+                    className={`${
+                      selectedMethod.cash
+                        ? " w-3 h-3 bg-Medium-Electric-Blue md:bg-Vivid-Red-Tangelo "
+                        : " bg-transparent"
+                    } rounded-full`}
                   />
                 </div>
+                <h3 className="font-semibold ml-3">Cash/Card to terminal</h3>
+              </div>
+              <div
+                onClick={() => {
+                  handlePaymentSelect("card");
+                }}
+                className="flex items-center cursor-pointer"
+              >
+                <div
+                  className={`${
+                    selectedMethod.card
+                      ? "  border-Medium-Electric-Blue md:border-Vivid-Red-Tangelo "
+                      : " border-Spanish-Gray"
+                  } rounded-full border-2 w-5 h-5 flex items-center justify-center`}
+                >
+                  <div
+                    className={`${
+                      selectedMethod.card
+                        ? " w-3 h-3 bg-Medium-Electric-Blue md:bg-Vivid-Red-Tangelo "
+                        : " bg-transparent"
+                    } rounded-full`}
+                  />
+                </div>
+                <h3 className="font-semibold ml-3">Pay now with card</h3>
               </div>
             </div>
+            <div className="md:hidden bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
+            {!selectedMethod.cash && (
+              <div className="space-y-2 md:my-4">
+                <input
+                  className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White text-xs md:bg-White"
+                  placeholder="Name on card"
+                />
+
+                <div className="w-full py-3 px-4 rounded-lg outline-none bg-Flash-White md:bg-White flex flex-row md:flex-col lg:flex-row items-center">
+                  <img src="./assets/credit-card.svg" alt="credit-card" />
+                  <div className="flex flex-row md:flex-col lg:flex-row items-center text-xs justify-between flex-1">
+                    <input
+                      className="bg-transparent outline-none w-20 md:w-28 ml-2 md:my-1 lg:my-0 md:ml-0 lg:ml-2"
+                      placeholder="Card number"
+                    />
+                    <div>
+                      <input
+                        className="bg-transparent outline-none max-w-[70px] mr-2 md:my-1 lg:my-0  border-r md:border-none lg:border-r border-Spanish-Gray"
+                        placeholder="MM/AA"
+                      />
+                      <input
+                        className="bg-transparent outline-none max-w-[40px] ml-2 md:my-1 md:ml-0 lg:ml-0 lg:my-0"
+                        placeholder="CVC"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="hidden md:block bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
           </div>
-        )}
-        <div className="hidden md:block bg-Spanish-Gray h-px max-w-[242px] mx-auto opacity-20 my-4" />
+        ) : null}
       </div>
 
       <div className="fixed bottom-0 left-0 right-0 md:static  p-4 md:p-0 md-p-0 md:mx-0 z-50 md:mb-4 md:my-4 bg-White md:bg-transparent rounded-lg shadow-lg justify-between md:shadow-none flex flex-row md:flex-col">
-        <div className="font-semibold text-xs flex md:flex-row flex-col md:items-center md:my-4 justify-between">
+        <div className="font-semibold text-xs flex md:flex-row flex-col md:items-center md:my-3 justify-between">
           <h2>TOTAL</h2>
           <h1 className="font-bold text-lg mt-2 md:mt-0">
             {cart
@@ -132,10 +135,12 @@ const Payment = () => {
             €
           </h1>
         </div>
-        <p className="hidden md:block bg-Pastel-Violet text-Pastel-Violet font-semibold p-4 text-xs bg-opacity-20 rounded-lg">
-          Your booking will be sent to restaurant name with whatsapp and you
-          will receive status notifications in chat.
-        </p>
+        {selectedOption === "Delivery" || selectedOption === "Take Away" ? (
+          <p className="hidden md:block bg-Pastel-Violet text-Pastel-Violet font-semibold p-4 text-xs bg-opacity-20 rounded-lg">
+            Your booking will be sent to restaurant name with whatsapp and you
+            will receive status notifications in chat.
+          </p>
+        ) : null}
         <button
           onClick={handleButtonClick}
           className="bg-Vivid-Red-Tangelo rounded-lg text-White font-semibold px-8 md:hidden text-sm"
